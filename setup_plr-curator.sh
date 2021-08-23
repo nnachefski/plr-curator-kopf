98fb1491c7c55f578302a55d41ee2c83e95c0d0d
Nick Nachefski <nick@arrowglance.com>
2021-08-23 17:02:38 -0500

    test

--- setup_plr-curator.sh
+++ setup_plr-curator.sh
@@ -7,7 +7,6 @@ oc apply -f plr-curator.crd
 # note: perms (apis-to-verbs) for the operator were scoped as tightly as possible
 cat plr-curator-kopf.yaml |sed "s/\$HARBOR_CFG/$HARBOR_CFG/" |oc apply -f -
 
-
 #if you want to peer a locally running operator: 
 # - pip-3.8 install kopf
 # - apply the peering CRDs (below)
